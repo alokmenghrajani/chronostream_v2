9080d1819c696bb23f5372085b3acc5b9fab3262
Alok Menghrajani <alok@squareup.com>
2018-07-27 09:25:09 -0700

    nit

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 for i in `seq 1 50`; do
-  java -cp /opt/nfast/java/classes/nCipherKM.jar:./chronostream-2.0.jar org.openjdk.jmh.Main -t $i -rff throughput_$i
-  java -cp /opt/nfast/java/classes/nCipherKM.jar:./chronostream-2.0.jar org.openjdk.jmh.Main -tu ms -bm avgt -t $i -rff latency_$i
+  echo "running $i"
+  java -cp /opt/nfast/java/classes/nCipherKM.jar:./chronostream-2.0.jar org.openjdk.jmh.Main -wi 1 -i 2 -f 2 -t $i -rff results/throughput_$i
+  java -cp /opt/nfast/java/classes/nCipherKM.jar:./chronostream-2.0.jar org.openjdk.jmh.Main -wi 1 -i 2 -f 2 -tu ms -bm avgt -t $i -rff results/latency_$i
 done
