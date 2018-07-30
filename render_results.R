b31547a2e547fce5905ce9c4cb35eb08d8d4b298
Alok Menghrajani <alok@squareup.com>
2018-07-30 10:47:37 -0700

    meh

--- render_results.R
+++ render_results.R
@@ -1,48 +1,48 @@
-setwd('results')
-
-threads = 35
+# Experiment 1: measure throughput/latency as the number of threads grows.
+# (in the past, we were seeing some perf degradation)
 
 # merge all the files
+threads = 50
 throughputs <- data.frame()
 latencies <- data.frame()
 
 for (thread in 1:threads) {
-  throughput <- read.table(paste('throughput_', thread, sep=''), header=TRUE, sep=',')
+  throughput <- read.table(paste('results/exp1/throughput_', thread, '.csv', sep=''), header=TRUE, sep=',')
   throughputs <- rbind(throughputs, throughput[c("Benchmark", "Threads", "Score")])
 
-  latency <- read.table(paste('latency_', thread, sep=''), header=TRUE, sep=',')
+  latency <- read.table(paste('results/exp1/latency_', thread, '.csv', sep=''), header=TRUE, sep=',')
   latencies <- rbind(latencies, latency[c("Benchmark", "Threads", "Score")])
 }
-
-# make per benchmark graphs
 throughputs$Benchmark <- sub(".*[.]", "", throughputs$Benchmark)
 latencies$Benchmark <- sub(".*[.]", "", latencies$Benchmark)
+
+# make per benchmark graphs
 benchmarks = c("testAesDecryption", "testHmac", "testRsaDecryption")
 for (benchmark in benchmarks) {
   jce = paste(benchmark, "Jce", sep='')
   ncore = paste(benchmark, "NCore", sep='')
-  png(filename=paste(benchmark, "_throughput.png", sep=''))
+  png(filename=paste('graphs/exp1_', benchmark, "_throughput.png", sep=''))
   throughput_jce <- throughputs[which(throughputs$Benchmark == jce),]
   throughput_ncore <- throughputs[which(throughputs$Benchmark == ncore),]
 
   par(mar=c(3, 7, 2, 1))
   yaxis = c(0, max(throughput_jce$Score, throughput_ncore$Score))
-  plot(throughput_jce$Score, axes=F, ylim=yaxis, xlab='', ylab='', type='l', main='', xlim=c(1, threads), col='blue')
-  points(throughput_jce$Score, pch=20, col='blue')
+  plot(throughput_jce$Threads, throughput_jce$Score, axes=F, ylim=yaxis, xlab='', ylab='', type='l', main='', col='blue')
+  points(throughput_jce$Threads, throughput_jce$Score, pch=20, col='blue')
   axis(2, ylim=yaxis, col='blue', lwd=2, line=3.5)
   mtext(2, text='throughput [ops/s]', line=5.5)
   par(new=T)
-  plot(throughput_ncore$Score, axes=F, ylim=yaxis, xlab='', ylab='', type='l', main='', xlim=c(1, threads), col='black')
-  points(throughput_ncore$Score, pch=20, col='black')
+  plot(throughput_ncore$Threads, throughput_ncore$Score, axes=F, ylim=yaxis, xlab='', ylab='', type='l', main='', col='black')
+  points(throughput_ncore$Threads, throughput_ncore$Score, pch=20, col='black')
 
   axis(1, pretty(c(1, threads)))
   mtext('threads', side=1, line=2)
 
-  legend("topleft", inset=.05, title=benchmark, c("JCE","NCore"), fill=c('blue', 'black'), horiz=TRUE)
   grid()
+  legend("topleft", bg='white', inset=.05, title=benchmark, c("JCE","NCore"), fill=c('blue', 'black'), horiz=TRUE)
   dev.off()
 
-  png(filename=paste(benchmark, "_latency.png", sep=''))
+  png(filename=paste('graphs/exp1_', benchmark, "_latency.png", sep=''))
   latency_jce <- latencies[which(latencies$Benchmark == jce),]
   latency_ncore <- latencies[which(latencies$Benchmark == ncore),]
 
@@ -59,7 +59,75 @@ for (benchmark in benchmarks) {
   axis(1, pretty(c(1, threads)))
   mtext('threads', side=1, line=2)
 
-  legend("topleft", inset=.05, title=benchmark, c("JCE","NCore"), fill=c('blue', 'black'), horiz=TRUE)
   grid()
+  legend("topleft", bg='white', inset=.05, title=benchmark, c("JCE","NCore"), fill=c('blue', 'black'), horiz=TRUE)
+  dev.off()
+}
+
+# Experiment 2: measure throughput/latency as the test runs longer.
+# (in the past, we were seeing some perf degradation)
+
+# merge all the files
+min_time = 10
+max_time = 120
+throughputs <- data.frame()
+latencies <- data.frame()
+for (t in seq(from=min_time, to=max_time, by=10)) {
+  throughput <- read.table(paste('results/exp2/throughput_', t, '.csv', sep=''), header=TRUE, sep=',')
+  throughput$Time <- t
+  throughputs <- rbind(throughputs, throughput[c("Benchmark", "Time", "Score")])
+
+  latency <- read.table(paste('results/exp2/latency_', t, '.csv', sep=''), header=TRUE, sep=',')
+  latency$Time <- t
+  latencies <- rbind(latencies, latency[c("Benchmark", "Time", "Score")])
+}
+throughputs$Benchmark <- sub(".*[.]", "", throughputs$Benchmark)
+latencies$Benchmark <- sub(".*[.]", "", latencies$Benchmark)
+
+# make per benchmark graphs
+benchmarks = c("testAesDecryption", "testHmac", "testRsaDecryption")
+for (benchmark in benchmarks) {
+  jce = paste(benchmark, "Jce", sep='')
+  ncore = paste(benchmark, "NCore", sep='')
+  png(filename=paste('graphs/exp2_', benchmark, "_throughput.png", sep=''))
+  throughput_jce <- throughputs[which(throughputs$Benchmark == jce),]
+  throughput_ncore <- throughputs[which(throughputs$Benchmark == ncore),]
+
+  par(mar=c(3, 7, 2, 1))
+  yaxis = c(0, ceiling(max(throughput_jce$Score, throughput_ncore$Score)))
+  plot(throughput_jce$Time, throughput_jce$Score, axes=F, ylim=yaxis, xlab='', ylab='', type='l', main='', col='blue')
+  points(throughput_jce$Time, throughput_jce$Score, pch=20, col='blue')
+  axis(2, ylim=yaxis, col='blue', lwd=2, line=3.5)
+  mtext(2, text='throughput [ops/s]', line=5.5)
+  par(new=T)
+  plot(throughput_ncore$Time, throughput_ncore$Score, axes=F, ylim=yaxis, xlab='', ylab='', type='l', main='', col='black')
+  points(throughput_ncore$Time, throughput_ncore$Score, pch=20, col='black')
+
+  axis(1, pretty(c(min_time, max_time)))
+  mtext('time [s]', side=1, line=2)
+
+  grid()
+  legend("bottomright", bg='white', inset=0.05, title=benchmark, c("JCE","NCore"), fill=c('blue', 'black'), horiz=TRUE)
+  dev.off()
+
+  png(filename=paste('graphs/exp2_', benchmark, "_latency.png", sep=''))
+  latency_jce <- latencies[which(latencies$Benchmark == jce),]
+  latency_ncore <- latencies[which(latencies$Benchmark == ncore),]
+
+  par(mar=c(3, 7, 2, 1))
+  yaxis = c(0, ceiling(max(latency_jce$Score, latency_ncore$Score)))
+  plot(latency_jce$Time, latency_jce$Score, axes=F, ylim=yaxis, xlab='', ylab='', type='l', main='', col='blue')
+  points(latency_jce$Time, latency_jce$Score, pch=20, col='blue')
+  axis(2, ylim=yaxis, col='blue', lwd=2, line=3.5)
+  mtext(2, text='latency [ms]', line=5.5)
+  par(new=T)
+  plot(latency_ncore$Time, latency_ncore$Score, axes=F, ylim=yaxis, xlab='', ylab='', type='l', main='', col='black')
+  points(latency_ncore$Time, latency_ncore$Score, pch=20, col='black')
+
+  axis(1, pretty(c(min_time, max_time)))
+  mtext('time [s]', side=1, line=2)
+
+  grid()
+  legend("bottomright", bg='white', inset=.05, title=benchmark, c("JCE","NCore"), fill=c('blue', 'black'), horiz=TRUE)
   dev.off()
 }
--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+mkdir results
+mkdir results/exp1
+mkdir results/exp2
+
 for i in `seq 1 50`; do
-  for j in `seq 10 10 120`; do
-      java -cp /opt/nfast/java/classes/nCipherKM.jar:./chronostream-2.0.jar org.openjdk.jmh.Main -wi 1 -r $j"s" -i 1 -f 1 -t $i -rff "results/throughput_"$i"_"$j".csv"
-      java -cp /opt/nfast/java/classes/nCipherKM.jar:./chronostream-2.0.jar org.openjdk.jmh.Main -wi 1 -r $j"s" -i 1 -f 1 -t $i -tu ms -bm avgt -rff "results/latency_"$i"_"$j".csv"
-  done
+  java -cp /opt/nfast/java/classes/nCipherKM.jar:./chronostream-2.0.jar org.openjdk.jmh.Main -wi 1 -r 30s -i 4 -f 2 -t $i -rff "results/exp1/throughput_"$i".csv"
+  java -cp /opt/nfast/java/classes/nCipherKM.jar:./chronostream-2.0.jar org.openjdk.jmh.Main -wi 1 -r 30s -i 4 -f 2 -t $i -tu ms -bm avgt -rff "results/exp1/latency_"$i".csv"
+done
+
+for j in `seq 10 10 120`; do
+  java -cp /opt/nfast/java/classes/nCipherKM.jar:./chronostream-2.0.jar org.openjdk.jmh.Main -wi 1 -r $j"s" -i 1 -f 1 -t 20 -rff "results/exp2/throughput_"$j".csv"
+  java -cp /opt/nfast/java/classes/nCipherKM.jar:./chronostream-2.0.jar org.openjdk.jmh.Main -wi 1 -r $j"s" -i 1 -f 1 -t 20 -tu ms -bm avgt -rff "results/exp2/latency_"$j".csv"
 done
